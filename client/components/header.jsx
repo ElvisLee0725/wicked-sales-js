3c0577df5c50abd4bd49917d2e5d4f6ad3d4e46c
ElvisLee0725 <ElvisLee0725@gmail.com>
2020-05-09 23:00:21 +0000

    Finished view products front end

---
+++ client/components/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        <i className="fas fa-dollar-sign"></i>&nbsp;Wicked Sales
+      </a>
+    </nav>
+  );
+}
+
+export default Header;
